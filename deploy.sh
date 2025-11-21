224d66471a6dd067148a10cefaea972498b6fc2f
Val Hovey <khoveyc@gmail.com>
2025-11-20 18:18:30 -0700

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin zsh
 
 npm run build
-cp -r dist/assets ../../Git/kylehovey.github.io
-cp dist/index.html ../../Git/kylehovey.github.io
+cp -r dist/assets ../../Git/valhovey.github.io
+cp dist/index.html ../../Git/valhovey.github.io
