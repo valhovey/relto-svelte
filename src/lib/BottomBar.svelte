4b535b7c12730d9a80ab2cdb395f07f10c1086fc
valhovey <khoveyc@gmail.com>
2026-01-01 23:34:26 -0700

    Updating link to not be subdirectory

--- src/lib/BottomBar.svelte
+++ src/lib/BottomBar.svelte
@@ -30,7 +30,7 @@
     <span class="divider">|</span>
     <a href="blog">Blog</a>
     <span class="divider">|</span>
-    <a href="Resolutions">Resolutions</a>
+    <a href="Resolutions">resolutions.html</a>
     <span class="divider">|</span>
     <a href="MATH_5620">Math 5620</a>
   </div>
